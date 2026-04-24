12f78daf95fb2fe5ebfb552119c4fc34a2631d3f
SuatOzcan <41187131+SuatOzcan@users.noreply.github.com>
2026-04-24 11:17:01 +0300

    Add TodoList.

---
+++ Components/TodoList.jsx
@@ -0,0 +1,16 @@
+import TodoItem from "./TodoItem";
+
+export default function TodoList({ todos, deleteTodo, updateTodo }) {
+  return (
+    <div>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          deleteTodo={deleteTodo}
+          updateTodo={updateTodo}
+        />
+      ))}
+    </div>
+  );
+}
